ca21842ebbed0d287bf6e77f58bc150fcf8c8760
alex_nemynov <alexnemynov@yandex.ru>
2023-11-25 19:27:58 +0300

    make migrations

--- products/models.py
+++ products/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)  # макс длина в символах; поля уникальны
-    descriprtion = models.TextField(null=True, blank=True)  # два способа задать пуст стр; поле Discription может быть пустым
+    description = models.TextField(null=True, blank=True)  # два способа задать пуст стр; поле Discription может быть пустым
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
