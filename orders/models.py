9d086e13ad3b5ce30ee758f31e53f419c28b290e
alex_nemynov <alexnemynov@yandex.ru>
2023-12-24 23:59:55 +0300

    added OrderModel, OrderForm, template success.html

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Order(models.Model):
+    CREATED = 0  # Статусы указывают в начале класса
+    PAID = 1
+    ON_WAY = 2
+    DELIVERED = 3
+    STATUSES = (
+        (CREATED, 'Создан'),
+        (PAID, 'Оплачен'),
+        (ON_WAY, 'В пути'),
+        (DELIVERED, 'Доставлен'),
+    )
+
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=256)
+    address = models.CharField(max_length=256)
+    basket_history = models.JSONField(default=dict)  # нельзя ссылаться на другие модели, чтобы поле не обновлялась
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.PositiveIntegerField(choices=STATUSES, default=CREATED)
+    initiator = models.ForeignKey(User, on_delete=models.CASCADE)  # если удалился пользователь, и заказа тоже удаляем
+
+    def __str__(self):
+        return f'Order #{self.id}. {self.first_name} {self.last_name}'
+
+
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.edit import CreateView
+from .forms import OrderForm
 
 
 class OrderCreateView(CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
 
 
