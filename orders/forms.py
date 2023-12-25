a6280cdc5d64dbc6fb1eb5f4da354c1ef9ca5bbb
alex_nemynov <alexnemynov@yandex.ru>
2023-12-25 22:57:10 +0300

    edited OrderCreateView, order-create.html. Added OrderAdmin. Added humanize intcomma to templates

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,15 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Иван'}))
+    last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Иванов'}))
+    email = forms.EmailField(label='Адрес электронной почты', widget=forms.EmailInput(attrs={
+        'class': 'form-control', 'placeholder': 'you@example.com'}))
+    address = forms.CharField(label='Адрес', widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Россия, Москва, ул. Мира, дом 6'}))
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address']
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,16 @@
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .forms import OrderForm
 
 
 class OrderCreateView(CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+    extra_context = {'title': 'Store - Оформление заказа'}
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super().form_valid(form)
 
 
